b042be9354d32682be03052e6e05e779725149a6
indranoojk <indranoojk@gmail.com>
2024-04-20 22:29:42 +0530

    edited reset index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const colors = require("colors");
-const connectToMongo = require('./db');
-var cors = require('cors');
-const { AdminRouter } = require('./routes/auth');
-const { ContractRouter } = require('./routes/contract');
-const { fetchadmin } = require('./middleware/fetchadmin');
 require('dotenv').config();
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors')
+const colors = require("colors");
 
+connectToMongo();
 const app = express();
 
 const port = process.env.PORT;
@@ -15,16 +13,10 @@ app.use(cors())
 app.use(express.json())
 
 
-app.use("/admin", AdminRouter);
-
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contract', require('./routes/contract'))
 
-
-app.use(fetchadmin)
-
-app.use("/contract", ContractRouter);
 
 
 app.listen(port, async () => {
--- routes/auth.js
+++ routes/auth.js
@@ -11,57 +11,57 @@ const JWT_SECRET = 'DevelopedbyIndr@';
 
 
 // Route 1: Creating an Admin using: POST "/api/auth/createadmin". 
-// AdminRouter.post('/createadmin',
-//  [
-//     body("name", "Enter a valid name").isLength({ min: 5 }),
-//     body("email", "Enter a valid email").isEmail(),
-//     body("password", "Password must be atleast 8 characters").isLength({ min: 8 })
-//  ],
-//  async (req, res) => {
-//     let success = false;
-//     // If there are errors, return Bad request and the errors
-//     const errors = validationResult(req);
-//     // When there is a email entered and that email is already exists in the DB then it will show status 400 (Bad request) and show the error
-//     if(!errors.isEmpty()){
-//         return res.status(400).json({success, errors: errors.array()});
-//     }
-//     try {
-//         // Check whether the user with this email exists already
-//         // By the findOne function it is checking for the specific email in the database if the email already present in the db then it will show status 400
-//         let admin = await Admin.findOne({ email: req.body.email });
-//         if(admin){
-//             return res.status(400).json({success, error: "Sorry an admin with this email already exists"});
-//         }
+AdminRouter.post('/createadmin',
+ [
+    body("name", "Enter a valid name").isLength({ min: 5 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 8 characters").isLength({ min: 8 })
+ ],
+ async (req, res) => {
+    let success = false;
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    // When there is a email entered and that email is already exists in the DB then it will show status 400 (Bad request) and show the error
+    if(!errors.isEmpty()){
+        return res.status(400).json({success, errors: errors.array()});
+    }
+    try {
+        // Check whether the user with this email exists already
+        // By the findOne function it is checking for the specific email in the database if the email already present in the db then it will show status 400
+        let admin = await Admin.findOne({ email: req.body.email });
+        if(admin){
+            return res.status(400).json({success, error: "Sorry an admin with this email already exists"});
+        }
 
-//         // Salt will generate some random string
-//         const salt = await bcrypt.genSalt(10);
-//         // This secPass will create a new secured password using hashing
-//         const secPass = await bcrypt.hash(req.body.password, salt);
+        // Salt will generate some random string
+        const salt = await bcrypt.genSalt(10);
+        // This secPass will create a new secured password using hashing
+        const secPass = await bcrypt.hash(req.body.password, salt);
 
-//         // Creating a new Admin
-//         admin = await Admin.create({
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: secPass,
-//         });
+        // Creating a new Admin
+        admin = await Admin.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass,
+        });
 
-//         const data = {
-//             admin: {
-//                 id: admin.id
-//             }
-//         }
+        const data = {
+            admin: {
+                id: admin.id
+            }
+        }
 
-//         const authtoken = jwt.sign(data, JWT_SECRET);
+        const authtoken = jwt.sign(data, JWT_SECRET);
 
-//         success = true;
-//         res.json({ success, authtoken });
-//     } catch(error) {
-//         // catch errors
-//         console.error(error.message);
-//         // status code says something is wrong
-//         res.status(500).send("Internal Server Error")
-//     }
-// });
+        success = true;
+        res.json({ success, authtoken });
+    } catch(error) {
+        // catch errors
+        console.error(error.message);
+        // status code says something is wrong
+        res.status(500).send("Internal Server Error")
+    }
+});
 
 
 // Route 2: Authenticate an Admin using: POST "/api/auth/login".
