b0950ebb88a2c00e5a4b3e1e4800922b4d3d6f12
indranoojk <indranoojk@gmail.com>
2024-04-20 21:15:14 +0530

    added connection command to mongo

--- db.js
+++ db.js
@@ -7,7 +7,12 @@ const mongoURL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG
 
 // Connecting to the database
 const connectToMongo = () => {
-    mongoose.connect(mongoURL);
+    mongoose.connect(mongoURL).then(() => {
+      console.log("Connected to MongoDB Successfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });;
 }
 
 module.exports = connectToMongo;
