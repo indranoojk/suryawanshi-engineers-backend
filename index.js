7c39d83a597f911c84aae5e23b12120b9cc7358c
indranoojk <indranoojk@gmail.com>
2024-04-21 11:21:18 +0530

    aded home route msg

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 var cors = require('cors')
 const colors = require("colors");
 
+connectToMongo();
 
 const app = express();
 
@@ -13,17 +14,27 @@ app.use(cors())
 app.use(express.json())
 
 
+
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ msg: "I am in home route" });
+  } catch (error) {
+    res.status(500).json({ msg: "Error in home route" });
+  }
+});
+
+
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contract', require('./routes/contract'))
 
 
 
 app.listen(port, async () => {
-    try {
-        await connectToMongo;
-        console.log(colors.bgYellow(`connectd to mongo db`));
-      } catch (error) {
-        console.log(colors.bgRed("Error in connecting mongoDb"));
-      }
+    // try {
+    //     await connectToMongo;
+    //     console.log(colors.bgYellow(`connectd to mongo db`));
+    //   } catch (error) {
+    //     console.log(colors.bgRed("Error in connecting mongoDb"));
+    //   }
       console.log(colors.rainbow(`Backend is running on port ${port}`));
 })
