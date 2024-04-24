cfc6adf3718380f241fb06ca475e412255e9e7e3
indranoojk <indranoojk@gmail.com>
2024-04-24 22:55:55 +0530

    added images.js

--- routes/image.js
+++ routes/image.js
@@ -2,12 +2,13 @@ const express = require("express")
 const Image = require("../models/Image")
 const multer = require('multer');
 const path = require('path');
+const images = require('../Images/')
 
 const router = express.Router()
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './Images/')
+    cb(null, `${images}`)
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
