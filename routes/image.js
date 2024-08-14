52cf2b6e10129e461501f0ac73bb0303e4732e42
indranoojk <indranoojk@gmail.com>
2024-08-14 12:49:11 +0530

    updated frontend for images 4

--- routes/image.js
+++ routes/image.js
@@ -7,7 +7,7 @@ var fetchadmin = require('../middleware/fetchadmin');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads');
+    cb(null, '../uploads');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
@@ -20,24 +20,29 @@ router.post('/upload', upload.single("image"), fetchadmin, async (req, res) => {
     try {
         const { path, filename } = req.file;
         const image = await ImageModel({path, filename});
-        await image.save();
-        res.send({"msg": "Image Uploaded"});
+        const savedImage = await image.save();
+        res.send({message: "Image Uploaded", imageId: savedImage._id});
     } catch (error) {
         res.send({"error": "Unable to upload image"});
     }
 })
 
 router.get("/image/:id", fetchadmin, async (req, res) => {
-    const {id} = req.params;
+    // const {id} = req.params;
     try {
-        const image = await ImageModel.findById(id);
-        if(!image){
-            res.send({"msg": "Image Not Found"});
+        // const image = await ImageModel.findById(id);
+        // if(!image){
+        //     res.send({"msg": "Image Not Found"});
+        // }
+
+        // const imagePath = path.join(__dirname, "uploads", image.filename);
+        // res.sendFile(imagePath);
+        const image = await ImageModel.findById(req.params.id);
+        if (!image) {
+            return res.status(404).json({ message: 'Image not found' });
         }
-
-        const imagePath = path.join(__dirname, "uploads", image.filename);
-        res.sendFile(imagePath);
-
+        res.set('Content-Type', image.contentType);
+        res.send(image.data);
     } catch (error) {
         res.send({"error": "Unable to get image"})
     }
