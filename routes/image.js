99b5c7c600e04b489f0527fa89a0371929ad1a40
indranoojk <indranoojk@gmail.com>
2024-08-14 11:13:53 +0530

    updated frontend for images 1

--- routes/image.js
+++ routes/image.js
@@ -7,7 +7,7 @@ var fetchadmin = require('../middleware/fetchadmin');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../../uploads');
+    cb(null, './uploads');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
